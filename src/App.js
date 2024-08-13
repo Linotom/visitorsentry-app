e0bcc5ee8fd39980beb53532df446b0a00b5f4ba
lino tom <linotom04@gmail.com>
2024-08-13 11:26:25 +0530

    Navbar  Created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import AddVisitor from './components/AddVisitor';
 import ViewAllVisitors from './components/ViewAllVisitors';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <AddVisitor/>
-      <ViewAllVisitors/>
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<AddVisitor/>}/>
+      <Route path='/Viewall' element={<ViewAllVisitors/>}/>
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
